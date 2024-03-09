21601b546e5432d7a5a62063177922c5a2bef712
munkherdene <mongolzkarigun@gmail.com>
2024-03-09 16:08:44 +0800

    google auth done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { session, ...rest } = pageProps;
+  
+  return (
+    <SessionProvider session={session}>
+      <Component {...rest} />
+    </SessionProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Component from "./component/LoginButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
   <div className=" h-full flex w-full  justify-center items-center  ">
-      <h1> hllo next js </h1>
-  </div>
+      {/* <h1> hllo next js </h1> */}
+      <div>
+    <Component />
+      </div>
+    </div>
   );
 }
